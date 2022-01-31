e813fd6ec7c3a3d94b4b7013d14fdaae472899b5
manrriqueMY <manrrique.my@gmail.com>
2022-01-31 00:35:18 -0500

    ok

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -12,7 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
+        http.cors().disable()
                 .csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
                 .and()
