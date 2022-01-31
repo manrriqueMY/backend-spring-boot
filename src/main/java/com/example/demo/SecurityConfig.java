85d6a54a0d9114faa8d39e3a5d63baa3997964ea
manrriqueMY <manrrique.my@gmail.com>
2022-01-31 01:08:09 -0500

    ok

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @EnableWebSecurity
@@ -13,28 +14,20 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().disable()
-                .csrf().disable()
-                .authorizeRequests().anyRequest().permitAll();
+        http.cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues()).and().csrf()
+                .disable()
+                .authorizeRequests().anyRequest().authenticated()
+                .and()
+                .httpBasic();
     }
-    /*
-     * @Override
-     * protected void configure(HttpSecurity http) throws Exception {
-     * http.cors().disable()
-     * .csrf().disable()
-     * .authorizeRequests().anyRequest().authenticated()
-     * .and()
-     * .httpBasic();
-     * }
-     */
-    /*
-     * @Autowired
-     * public void configureGlobal(AuthenticationManagerBuilder auth)
-     * throws Exception {
-     * auth.inMemoryAuthentication()
-     * .withUser("admin")
-     * .password("{noop}password")
-     * .roles("USER");
-     * }
-     */
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("USER");
+    }
+
 }
